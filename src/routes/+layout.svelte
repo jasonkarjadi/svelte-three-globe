9a74225db035c8d5812f0a2a6db7a62d80e2ddf9
jason@karjadi.com <jason@karjadi.com>
2023-01-11 15:45:01 +0700

    add styling and icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,11 @@
   import { onMount } from "svelte";
   import {
     AmbientLight,
+    Color,
     DirectionalLight,
     PerspectiveCamera,
     Raycaster,
     Scene,
-    Vector2,
     WebGLRenderer,
   } from "three";
   import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
@@ -14,20 +14,22 @@
   import "./styles.scss";
 
   let canvas: HTMLCanvasElement,
-    width = 0,
-    height = 0,
-    requestID = 0;
+    innerWidth = 0,
+    innerHeight = 0,
+    requestID = 0,
+    offsetWidth = 32,
+    offsetHeight = 120;
 
   const scene = new Scene(),
     camera = new PerspectiveCamera(50, 2, 1, 5000),
-    mouse = new Vector2(),
     raycaster = new Raycaster(),
     gltfLoader = new GLTFLoader(),
     directionalLight = new DirectionalLight(0xffffff, 2),
-    ambientLight = new AmbientLight(0x404040, 2);
+    ambientLight = new AmbientLight(0xedf2f7, 1);
 
-  camera.position.set(0, 0, 1500);
-  directionalLight.position.y += 1000;
+  camera.position.setZ(2000);
+  directionalLight.position.y += 800;
+  scene.background = new Color(0x0e0e10);
   scene.add(camera.add(directionalLight), ambientLight);
 
   onMount(() => {
@@ -57,9 +59,9 @@
     );
 
     const resize = () => {
-      camera.aspect = width / height;
+      camera.aspect = (innerWidth - offsetWidth) / (innerHeight - offsetHeight);
       camera.updateProjectionMatrix();
-      renderer.setSize(width, height);
+      renderer.setSize(innerWidth - offsetWidth, innerHeight - offsetHeight);
       renderer.setPixelRatio(devicePixelRatio);
     };
 
@@ -79,50 +81,77 @@
   });
 </script>
 
-<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+<svelte:window bind:innerWidth bind:innerHeight />
 
 <header>
-  <nav>
-    <a href="/">HOME</a>
+  <nav class="topleft">
+    <button><i class="fa-solid fa-bars" /></button>
   </nav>
+  <div class="topmiddle" />
+  <div class="topright"><button><i class="fa-solid fa-gear" /></button></div>
 </header>
 <main>
-  <canvas
-    bind:this={canvas}
-    on:mousemove={(e) =>
-      mouse.set((e.clientX / width) * 2 - 1, -(e.clientY / height) * 2 + 1)}
-    on:mousedown={() => {
-      raycaster.setFromCamera(mouse, camera);
-      let hit;
-      if (hit) {
-      }
-    }}
-  />
+  <canvas bind:this={canvas} />
 </main>
-<footer>
-  <small>© Jason Karjadi 2022. All rights reserved</small>
-</footer>
 <slot />
+<footer>
+  <button on:click={() => {}}>
+    {#if false}
+      Open
+    {:else}
+      <i class="fa-solid fa-rectangle-xmark" />
+      Close
+    {/if}
+  </button>
+</footer>
 
 <style lang="scss">
+  $my-border: solid #2d3748 1px;
+  $color-jetblack: #0e0e10;
+
   header,
   footer {
-    position: absolute;
-    left: 0;
-    right: 0;
     height: 3rem;
-    background-color: rgba($color: #00ffff, $alpha: 0.2);
     display: flex;
-    align-items: center;
-    padding: 0 3rem;
-    z-index: 2;
   }
-
   header {
-    top: 0;
-  }
+    > .topleft {
+      button {
+        border-right: $my-border;
+        border-radius: 0 0 1rem 0;
+      }
+    }
 
+    > .topmiddle {
+      flex: 1;
+    }
+
+    > .topright {
+      button {
+        border-radius: 0 0 0 1rem;
+        border-left: $my-border;
+      }
+    }
+
+    button {
+      height: 100%;
+      width: 4rem;
+      box-shadow: 0 2px 5px $color-jetblack;
+      border-bottom: $my-border;
+    }
+  }
   footer {
-    bottom: 0;
+    > button {
+      flex: 1;
+      border-radius: 1rem 1rem 0 0;
+      box-shadow: 0 -2px 5px $color-jetblack;
+      border-top: $my-border;
+    }
+  }
+  canvas {
+    margin: 1rem auto 0.5rem;
+    border-radius: 1rem;
+    box-shadow: 0 0 5px 2px $color-jetblack;
+    border: $my-border;
   }
 </style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<h2>Resources</h2>
+<ul>
+  <li>
+    <a href="https://solarsystem.nasa.gov/resources/2393/earth-3d-model/">
+      Globe 3D Model from NASA
+    </a>
+  </li>
+</ul>
+<small>© Jason Karjadi 2023. All rights reserved</small>
